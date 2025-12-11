2237cbc8b286c3a1a266ecf888905dec129a187b
Fajrul Fallah <fallahfajrul21@gmail.com>
2025-12-11 14:45:38 +0700

    Update doughnut

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -22,9 +22,7 @@ class Report extends Model
         'report_date' => 'datetime',
     ];
 
-    /**
-     * SATU Laporan (Report) DIMILIKI OLEH SATU Airport
-     */
+
     public function airport()
     {
         return $this->belongsTo(Airport::class, 'airport_id', 'id');
